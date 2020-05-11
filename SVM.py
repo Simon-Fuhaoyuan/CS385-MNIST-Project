c26d109a9fec1607c6e639c5f740c9ae760538c3
Simon-Fuhaoyuan <Simon-Fuhaoyuan@sjtu.edu.cn>
2020-05-12 00:08:26 +0800

    more experiments

--- SVM.py
+++ SVM.py
@@ -4,11 +4,15 @@ from MnistData import load_mnist
 
 x_train, y_train = load_mnist()
 x_test, y_test = load_mnist(kind='t10k')
-train_num = 10000
+train_num = 6000
 test_num = 1000
 
 
-predictor = svm.SVC(gamma='scale', C=1.0, decision_function_shape='ovr', kernel='rbf')
+predictor = svm.SVC(
+    gamma='scale', 
+    C=1.0, 
+    decision_function_shape='ovr', 
+    kernel='sigmoid')
 predictor.fit(x_train[:train_num], y_train[:train_num])
 result = predictor.predict(x_test[:test_num])
 accurancy = np.sum(np.equal(result, y_test[:test_num])) / test_num
